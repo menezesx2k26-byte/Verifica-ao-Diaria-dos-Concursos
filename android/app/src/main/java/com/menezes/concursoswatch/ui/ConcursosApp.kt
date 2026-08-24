bdd3aabe441ae0575de30437e5c5588efc373360
menezesx2k26-byte <menezesx2k26@gmail.com>
2026-08-23 21:37:34 -0300

    fix(android): import dp for navigation elevation

--- android/app/src/main/java/com/menezes/concursoswatch/ui/ConcursosApp.kt
+++ android/app/src/main/java/com/menezes/concursoswatch/ui/ConcursosApp.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
