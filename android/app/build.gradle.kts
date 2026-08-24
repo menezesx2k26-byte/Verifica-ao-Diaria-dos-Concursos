0d66f342c6b45e11023955936dce396eeee1fc5f
menezesx2k26-byte <menezesx2k26@gmail.com>
2026-08-24 07:58:07 -0300

    test(android): provide real JSON implementation to JVM tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.room:room-testing:2.6.1")
+    testImplementation("org.json:json:20240303")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2025.01.00"))
